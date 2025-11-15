2aa29f03d043a680e28593e97e223417303cbcc2
Guy Matz <gmatz@matz.org>
2025-11-15 16:44:03 +0100

    verlet 2 **seems** to be working?

--- verlet/lennard_jones.f95
+++ verlet/lennard_jones.f95
@@ -6,22 +6,28 @@ module lennard_jones
 
   CONTAINS
 
-  function dist(x, y, z) result(r)
-    real :: x, y, z, r
-    r = sqrt(x**2 + y**2 + z**2)
-  end function dist
+!  function v_prime_lj(epsilon, sigma, r_ab) result(v_prime)
+!    real, intent(in) :: epsilon, sigma
+!    real, intent(in) :: r_ab
+!    real, intent(out) :: v_prime
+!    !v_prime = 4 * epsilon * (-12 * sigma**12 / r_ab**13 + 6 * sigma**6 / r_ab**7 )
+!  end function v_prime_lj
 
-  function v_prime_lj(epsilon, sigma, r) result(v_prime)
-    real, intent(in) :: epsilon, sigma, r
-    real :: v_prime
-    v_prime = 4 * epsilon * (-12 * sigma**12 / r**13 + 6 * sigma**6 / r**7 )
-  end function v_prime_lj
-
-  function lj(epsilon, sigma, particle_a, particle_b) result(f_a)
+  function lj(epsilon, sigma, particle_a, particle_b, axis) result(f_a)
+    integer :: axis
     real, intent(in) :: epsilon, sigma
-    real (KIND=wp), INTENT(IN) :: particle_a, particle_b
+    real (KIND=wp), dimension(3), INTENT(IN) :: particle_a, particle_b
     real :: f_a
-    !f_a = - (dist(particle_a, particle_b))
+    real :: axes(3)
+    real :: x_ab, y_ab, z_ab
+    real :: r_ab
+    x_ab = particle_a(1) - particle_b(1)
+    y_ab = particle_a(2) - particle_b(2)
+    z_ab = particle_a(3) - particle_b(3)
+    axes = (/ x_ab, y_ab, z_ab /)
+    r_ab = sqrt( x_ab**2 + y_ab**2 + z_ab**2 )
+    !f_a = - ( axes(axis) / r_ab ) * v_prime_lj(epsilon, sigma, r_ab)
+
   end function lj
  
 end module lennard_jones
