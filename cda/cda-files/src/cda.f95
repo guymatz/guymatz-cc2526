1a699519eb78b32c70828b5993b75efe4630daf8
Guy Matz <gmatz@matz.org>
2025-11-28 21:41:31 +0100

    progress!
    Must implement:
      cube_int
      cube_del
      cube_cdz

--- cda/cda-files/src/cda.f95
+++ cda/cda-files/src/cda.f95
@@ -3,10 +3,26 @@ PROGRAM cda
   USE cubes
   IMPLICIT NONE
 
-  ! 1. Insantiate cube objects with data from cube files ../test/CuCO+/ab.cube, ../test/CuCO+/a.cube, and ../test/CuCO+/b.cube
+  ! 1. Insantiate cube objects with data from cube files
+  ! ../test/CuCO+/ab.cube, ../test/CuCO+/a.cube, and ../test/CuCO+/b.cube
+  TYPE(cube) :: rho_a, rho_b, rho_ab, rho_ref, drho
+  REAL (KIND=wp), DIMENSION(:), ALLOCATABLE :: cdz
+  call cube_get(rho_a, '../test/CuCO+/a.cube')
+  call cube_get(rho_b, '../test/CuCO+/b.cube')
+  call cube_get(rho_ab, '../test/CuCO+/ab.cube')
+  !call cube_print(rho_b)
 
-  ! 2. Generate a cube object containing the charge redistribution \Delta \rho = \rho_{AB} - \rho_{A} - \rho_{B}
+  ! 2. Generate a cube object containing the charge redistribution:
+  ! \Delta \rho = \rho_{AB} - \rho_{A} - \rho_{B}
+  call cube_gen_charge_redistribution(drho, rho_ab, rho_a, rho_b)
+  !call cube_print(drho)
 
-  ! 3. Use an ad hoc defined external procedures in module cubes for extracting the charge-displacement function from the charge redistribution
+  ! 3. Use an ad hoc defined external procedures in module cubes for extracting
+  ! the charge-displacement function from the charge redistribution
+  rho_ref = rho_a + rho_b
+  call cube_print(rho_ref)
+  drho = rho_ab - rho_ref
+  call cube_print(drho)
+!  cdz = cube_cdz(drho)
 
 END PROGRAM cda
--- cda/cda-files/src/cubes.f95
+++ cda/cda-files/src/cubes.f95
@@ -4,7 +4,13 @@ MODULE cubes
 
   TYPE, PUBLIC :: cube
     PRIVATE
-    CHARACTER (LEN=72) :: str1
+    CHARACTER (LEN=72) :: data_title = 'Untitled'
+    CHARACTER (LEN=72) :: data_desc = 'No Description'
+    real (KIND=wp) :: xmin, ymin, zmin, dx, dy, dz
+    integer :: nx, ny, nz, natom = 0
+    integer, DIMENSION(:), POINTER :: zahl
+    real (KIND=wp), DIMENSION(:), POINTER :: chrg, x, y, z
+    real (KIND=wp), DIMENSION(:), POINTER :: array
     ! fill
   END TYPE cube
 
@@ -13,7 +19,9 @@ MODULE cubes
             cube_sub, &
             cube_int, &
             cube_cdz, &
-            cube_del
+            cube_del, &
+            cube_gen_charge_redistribution, &
+            cube_print
 
   INTERFACE OPERATOR(+)
     MODULE PROCEDURE cube_add
@@ -29,23 +37,62 @@ MODULE cubes
   SUBROUTINE cube_get (mycube, infile)
     CHARACTER(LEN=*), INTENT(IN) :: infile
     TYPE (cube), INTENT(OUT) :: mycube
-    ! ...
     ! access attributes as follows:
-    ! mycube%str1 = ...
+    ! mycube%data_title = ...
+    ! temp var for unwannted data
+    real :: tmp
+    integer :: i, x, y, z
+    OPEN (UNIT=11, FILE=infile, STATUS="old", ACTION="read")
+    read(11, '(A)') mycube%data_title
+    read(11, '(A)') mycube%data_desc
+    read(11, FMT=*) mycube%natom, mycube%xmin, mycube%ymin, mycube%zmin
+    read(11, FMT=*) mycube%nx, mycube%dx, tmp, tmp
+    read(11, FMT=*) mycube%ny, tmp, mycube%dy, tmp
+    read(11, FMT=*) mycube%nz, tmp, tmp, mycube%dz
+    ! 2 x 5 array - data looks like
+    ! ZNatoms, chargeNatoms, xNatoms, yNatoms, zNatoms
+    allocate(mycube%zahl(mycube%natom))
+    allocate(mycube%chrg(mycube%natom))
+    allocate(mycube%x(mycube%natom))
+    allocate(mycube%y(mycube%natom))
+    allocate(mycube%z(mycube%natom))
+    do i = 1, mycube%natom, 1
+      read(11, FMT=*) mycube%zahl(i), mycube%chrg(i), mycube%x(i), mycube%y(i), mycube%z(i)
+    end do
+    ! slurp the rest of the file into c%array
+    allocate(mycube%array(mycube%nx * mycube%ny * mycube%nz))
+    read(11, FMT=*) mycube%array
   END SUBROUTINE cube_get
 
+  subroutine cube_print(c)
+    TYPE(cube), INTENT(IN) :: c
+    integer :: i, ctr = 4
+    print *, 'Title: ', c%data_title
+    print *, 'Desc.: ', c%data_desc
+    print *, 'Num Atoms: ', c%natom
+    print *, 'Atoms:'
+    do i = 1, c%natom, 1
+        print *, c%zahl(i), c%chrg(i), c%x(i), c%y(i), c%z(i)
+    end do
+    print '("First ", I0, " of Array:")', ctr 
+    do i = 1, ctr, 1
+        print *, c%array(i)
+    end do
+  end subroutine cube_print
 
   FUNCTION cube_add (mycube1, mycube2)
     TYPE(cube) :: cube_add
     TYPE(cube), INTENT(IN) :: mycube1, mycube2
-    ! ...
+    allocate(cube_add%array(size(mycube1%array)))
+    cube_add%array = mycube1%array + mycube2%array
   END FUNCTION cube_add
 
 
   FUNCTION cube_sub (mycube1, mycube2)
     TYPE(cube) :: cube_sub
     TYPE(cube), INTENT(IN) :: mycube1, mycube2
-    ! ...
+    allocate(cube_sub%array(size(mycube1%array)))
+    cube_sub%array = mycube1%array - mycube2%array
   END FUNCTION cube_sub
 
 
@@ -61,5 +108,20 @@ MODULE cubes
     ! ...
   END SUBROUTINE cube_del
 
+  ! Take in input a cube variable and returning a one-dimensional array
+  ! containing the values of Δq
+  SUBROUTINE cube_cdz (mycube)
+    TYPE (cube), INTENT(IN) :: mycube
+    ! ...
+  END SUBROUTINE cube_cdz
+
+  ! Take 4 cubes - a reference, A, B, & AB  - and set the reference%array
+  ! to AB%array - A%array - B%array
+  SUBROUTINE cube_gen_charge_redistribution (delta, AB, A, B)
+    TYPE (cube), INTENT(IN) :: AB, A, B
+    TYPE (cube), INTENT(OUT) :: delta
+    allocate(delta%array(size(AB%array)))
+    delta%array = AB%array - A%array - B%array
+  END SUBROUTINE cube_gen_charge_redistribution
 
 END MODULE cubes
