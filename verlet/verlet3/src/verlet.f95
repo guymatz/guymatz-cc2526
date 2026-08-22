da66c2551898700f7561bc710f82809e957c1b59
Guy Matz <gmatz@matz.org>
2026-08-22 13:08:29 -0400

    commented out write statements in main

--- verlet/verlet3/src/verlet.f95
+++ verlet/verlet3/src/verlet.f95
@@ -119,11 +119,11 @@ contains
         integer :: dimn, atom_i
 
       ! meat
-        write(stderr,*) "----- START compute_forces"
+      ! write(stderr,*) "----- START compute_forces"
         ! Step 1 of calculating forces: - get euclidean distances between points
-        write(stderr,*) "points: "
+      ! write(stderr,*) "points: "
         do atom_i = 1, 3, 1
-            write(stderr,*) "    ", atom_i, points(atom_i, :)
+          ! write(stderr,*) "    ", atom_i, points(atom_i, :)
         end do
         call get_ser(points(1, :), points(2, :), points(3, :), d_)
         ! write(stderr,*) "    DIST: ", d_
@@ -137,11 +137,11 @@ contains
         ! ! Step 3 of calculating forces: - get euclidean distances between points + delta
         ! do dimn = 1, 3, 1
         !     delta_d_(1, dimn) = eudist_with_delta(points(1, :), points(2, :), delta, dimn)
-        !     write(stderr,*) "Distance between A & B in", dimn,":", delta_d_(1, dimn)
+        !   ! write(stderr,*) "Distance between A & B in", dimn,":", delta_d_(1, dimn)
         !     delta_d_(2, dimn) = eudist_with_delta(points(1, :), points(3, :), delta, dimn)
-        !     write(stderr,*) "Distance between A & C in", dimn,":", delta_d_(2, dimn)
+        !   ! write(stderr,*) "Distance between A & C in", dimn,":", delta_d_(2, dimn)
         !     delta_d_(3, dimn) = eudist_with_delta(points(2, :), points(3, :), delta, dimn)
-        !     write(stderr,*) "Distance between B & C in", dimn,":", delta_d_(3, dimn)
+        !   ! write(stderr,*) "Distance between B & C in", dimn,":", delta_d_(3, dimn)
         ! end do
 
         ! Atom A-B xyz
@@ -170,11 +170,11 @@ contains
         delta_d_(6, 3) = eudist_with_delta(points(3, :), points(2, :), delta, 3)
 
         do atom_i = 1, 6, 1
-           write(stderr,*) "Delta D: ", atom_i, delta_d_(atom_i, :)
+         ! write(stderr,*) "Delta D: ", atom_i, delta_d_(atom_i, :)
         end do
 
         do atom_i = 1, 3, 1
-           write(stderr,*) "      D: ", atom_i, d_(atom_i)
+         ! write(stderr,*) "      D: ", atom_i, d_(atom_i)
         end do
 
         ! Step 3.5: I don't think I need to do this, but let's initialize the forces to 0
@@ -192,64 +192,64 @@ contains
         call jpca15(delta_ser, delta_er, delta_der)
         forces(1, 1) = (delta_er - er) / delta
 
-       write(stderr,*) "A, x                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(1, 1):", forces(1, 1)
+     ! write(stderr,*) "A, x                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(1, 1):", forces(1, 1)
 
       ! y
         delta_ser = (/delta_d_(1, 2), delta_d_(2, 2), d_(3)/)
         call jpca15(delta_ser, delta_er, delta_der)
         forces(1, 2) = (delta_er - er) / delta
 
-       write(stderr,*) "A, y                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(1, 2):", forces(1, 2)
+     ! write(stderr,*) "A, y                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(1, 2):", forces(1, 2)
 
       ! z
         delta_ser = (/delta_d_(1, 3), delta_d_(2, 3), d_(3)/)
         call jpca15(delta_ser, delta_er, delta_der)
         forces(1, 3) = (delta_er - er) / delta
 
-       write(stderr,*) "A, z                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(1, 3):", forces(1, 3)
+     ! write(stderr,*) "A, z                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(1, 3):", forces(1, 3)
 
     ! Atom 2 jiggled
         delta_ser = (/delta_d_(3, 1), d_(2), delta_d_(4, 1)/)
         call jpca15(delta_ser, delta_er, delta_der)
         forces(2, 1) = (delta_er - er) / delta
 
-       write(stderr,*) "B, x                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(2, 1):", forces(2, 1)
+     ! write(stderr,*) "B, x                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(2, 1):", forces(2, 1)
 
         delta_ser = (/delta_d_(3, 2), d_(2), delta_d_(4, 2)/)
         call jpca15(delta_ser, delta_er, delta_der)
         forces(2, 2) = (delta_er - er) / delta
 
-       write(stderr,*) "B, y                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(2, 2):", forces(2, 2)
+     ! write(stderr,*) "B, y                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(2, 2):", forces(2, 2)
 
         delta_ser = (/delta_d_(3, 3), d_(2), delta_d_(4, 3)/)
         call jpca15(delta_ser, delta_er, delta_der)
         forces(2, 3) = (delta_er - er) / delta
 
-       write(stderr,*) "B, z                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(2, 3):", forces(2, 3)
+     ! write(stderr,*) "B, z                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(2, 3):", forces(2, 3)
 
     ! Atom 3 jiggled
       ! x
@@ -257,40 +257,40 @@ contains
         call jpca15(delta_ser, delta_er, delta_der)
         forces(3, 1) = (delta_er - er) / delta
 
-       write(stderr,*) "C, x                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(3, 1):", forces(3, 1)
+     ! write(stderr,*) "C, x                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(3, 1):", forces(3, 1)
 
       ! y
         delta_ser = (/d_(1), delta_d_(5, 2), delta_d_(6, 2)/)
         call jpca15(delta_ser, delta_er, delta_der)
         forces(3, 2) = (delta_er - er) / delta
 
-       write(stderr,*) "C, y                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(3, 2):", forces(3, 2)
+     ! write(stderr,*) "C, y                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(3, 2):", forces(3, 2)
 
       ! z
         delta_ser = (/d_(1), delta_d_(5, 3), delta_d_(6, 3)/)
         call jpca15(delta_ser, delta_er, delta_der)
         forces(3, 3) = (delta_er - er) / delta
 
-       write(stderr,*) "C, z                          ", delta_ser
-       write(stderr,*) "  delta_ser:", delta_ser
-       write(stderr,*) "  delta_er:", delta_er
-       write(stderr,*) "  er:", er
-       write(stderr,*) "  forces(3, 3):", forces(3, 3)
+     ! write(stderr,*) "C, z                          ", delta_ser
+     ! write(stderr,*) "  delta_ser:", delta_ser
+     ! write(stderr,*) "  delta_er:", delta_er
+     ! write(stderr,*) "  er:", er
+     ! write(stderr,*) "  forces(3, 3):", forces(3, 3)
 
         ! do atom_i = 1, 3, 1
        !  !write(stderr,*) "Atom:", atom_i, forces(atom_i, :)
         ! end do
 
         ! DONE!
-        write(stderr,*) "----- END compute_forces"
+      ! write(stderr,*) "----- END compute_forces"
     end subroutine compute_force
 
 end module verlet
